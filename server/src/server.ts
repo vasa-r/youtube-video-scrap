69f3b4e79320674328c10f9bc19bfb9e311b49f6
VasaR <vasanthias52@gmail.com>
2025-03-31 22:31:36 +0530

    feat: response handlers for both error and success and error handler middleware added

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,13 +1,25 @@
 import express from "express";
 import "dotenv/config";
+import morgan from "morgan";
+import cors from "cors";
+
+// Configuration & Logging
 import { envConfig } from "./config/env-config";
-import logger from "./logger/logger";
 import { connectDb } from "./config/db";
+import logger, { stream } from "./logger/logger";
+
+// Utility Functions & Constants
+import { successRes } from "./utils/response-format";
+import { statusCode } from "./types/types";
+
+// Middlewares
+import errorHandler from "./middleware/error-handler";
+
+// Routes
+import v1 from "./routes/v1-route";
 
 const app = express();
 const PORT = process.env.PORT || 8000;
-
-app.use(express.json());
 
 const initServer = async () => {
   try {
@@ -15,7 +27,6 @@ const initServer = async () => {
 
     app.listen(PORT, () => {
       logger.info(`Server is up and running on port: ${PORT}`);
-      logger.info(envConfig.database.url);
     });
   } catch (error) {
     logger.error("Error starting server.");
@@ -23,12 +34,28 @@ const initServer = async () => {
   }
 };
 
+app.use(cors());
+app.use(express.json());
+app.use(
+  morgan(envConfig.server.env === "development" ? "dev" : "combined", {
+    stream,
+  })
+);
+
 app.get("/", (_, res) => {
-  res.status(200).json({
-    success: true,
-    message: "Hey there. You're good to go.",
+  res.status(statusCode.OK).json(successRes("Yo! Your API is up N running"));
+});
+
+app.use("api/v1", v1);
+
+app.use("*", (req, res) => {
+  res.status(statusCode.NOT_FOUND).json({
+    success: false,
+    message: `Endpoint not found - ${req.originalUrl}`,
   });
 });
+
+app.use(errorHandler);
 
 initServer().catch((err) => {
   logger.error(err);
